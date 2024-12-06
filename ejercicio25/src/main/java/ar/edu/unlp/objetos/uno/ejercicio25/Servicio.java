86d262a779d81c0084e5fbd606c437ac3c469efb
hiroshimaxtcz <121917968+hiroshimaxtcz@users.noreply.github.com>
2024-12-06 09:35:28 -0300

    Update Servicio.java

--- ejercicio25/src/main/java/ar/edu/unlp/objetos/uno/ejercicio25/Servicio.java
+++ ejercicio25/src/main/java/ar/edu/unlp/objetos/uno/ejercicio25/Servicio.java
@@ -4,7 +4,7 @@ public abstract class Servicio {
 
 	public abstract double DevolverMonto();
 	public boolean cumpleFecha(LocalDate fecha) {
-		if (LocalDate.now()== fecha) {
+		if (this.getFecha()== fecha) {
 			return true;
 		}
 		else
